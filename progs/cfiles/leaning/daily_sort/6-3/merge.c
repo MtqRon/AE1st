0b36b08bd3cce522b59a5db68692440580ba3bff
MtqRon <ae25kuniyasu@g.kumamoto-nct.ac.jp>
2025-06-03 06:09:56 +0900

    add daily_6_3

---
+++ progs/cfiles/leaning/daily_sort/6-3/merge.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+void merge_sort_rec(int *a, int *out, size_t left, size_t right){
+    if(right - left < 2) return;
+    
+    size_t mid = (right - left) / 2 + left;
+
+    merge_sort_rec(a, out, left, mid);
+    merge_sort_rec(a, out, mid, right);
+
+    size_t i = left, j = mid, k = left;
+
+    while(i < mid && j < right) out[k++] = (a[i] < a[j]) ? a[i++] : a[j++];
+    while(i < mid) out[k++] = a[i++];
+    while(j < right) out[k++] = a[j++];
+
+    for(k = left; k < right; k++) a[k] = out[k];
+}
+
+void merge_sort(int *a, size_t n){
+    if(n < 2) return;
+
+    int *out = malloc(n * sizeof(*out));
+    if(!out) return;
+
+    merge_sort_rec(a, out, 0, n);
+
+    free(out);
+}
---
+++ progs/cfiles/leaning/daily_sort/6-3/radix.c
@@ -0,0 +1,45 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int calc_digit(int *a, size_t i, size_t exp, int k){return (int)(a[i] / exp) % k;}
+
+void counting_digit_sort(int *a, size_t n, size_t *cnt, int *out, size_t exp, int k){
+    size_t i;
+
+    for(i = 0; i < n; i++) cnt[calc_digit(a, i, exp, k)]++;
+
+    for(i = 1; i < k; i++) cnt[i] += cnt[i - 1];
+
+    for(i = n; i > 0; i--){
+        int key = a[i - 1];
+        out[--cnt[calc_digit(a, i - 1, exp, k)]] = key;
+    }
+
+    for(i = 0; i < n; i++) a[i] = out[i];
+}
+
+void radix_sort(int *a, size_t n){
+    if(n < 2) return;
+
+    int k = 10;
+    size_t max = 0; 
+    size_t exp = 1;
+
+    size_t *cnt = calloc(k, sizeof(*cnt));
+    if(!cnt) return;
+
+    int *out = malloc(n * sizeof(*out));
+    if(!out){free(out); return;}
+
+    size_t i;
+
+    for(i = 0; i < n; i++) max = (max < a[i]) ? a[i] : max;
+
+    for(exp = 1; max / exp > 0; exp *= 10){
+        memset(cnt, 0, k * sizeof(*cnt));
+        counting_digit_sort(a, n, cnt, out, exp, k);
+    }
+
+    free(cnt); free(out);
+}
