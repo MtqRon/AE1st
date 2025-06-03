88ad1749b7c7ed80c7679bd58efc180382b1a227
MtqRon <ci20kuniyasu@g.kumamoto-nct.ac.jp>
2025-06-03 11:07:35 +0900

    change

---
+++ progs/hobby/get_bonus/twitch_config.py
@@ -0,0 +1,9 @@
+# Twitch API設定ファイル
+# このファイルに実際のAPI認証情報を入力してください
+
+CLIENT_ID = "mqalh26s3xf0hbfidughclvwt4x175"
+CLIENT_SECRET = "vvik1nqlgaqlelazuo0lb9p58tjl7z"
+
+# 監視設定
+STREAMER_NAME = "nyancakew"
+CHECK_INTERVAL = 180  # チェック間隔（秒）
