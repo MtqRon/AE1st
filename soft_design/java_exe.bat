8690011738ee7dc74e81de338d8d7d632d8a071c
MtqRon <ci20kuniyasu@g.kumamoto-nct.ac.jp>
2025-06-13 11:30:20 +0900

    a

--- soft_design/java_exe.bat
+++ soft_design/java_exe.bat
@@ -1,6 +1,6 @@
 setlocal
 set /p reponame="Enter package name>>>"
-cd "C:\Users\NikoHam\Desktop\AE1st\soft_design\%reponame%-MtqRon"
+cd "C:\Users\NikoHam\Desktop\AE1st\soft_design\%reponame%-Jun13-MtqRon"
 javac -d . %reponame%\*.java
 set /p class_name="Enter class name>>>"
 java %reponame%.%class_name%
