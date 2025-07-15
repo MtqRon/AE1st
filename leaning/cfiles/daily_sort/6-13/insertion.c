3c65f5aaf808dfb991a0b03e617acf9c646cfc1c
MtqRon <ae25kuniyasu@g.kumamoto-nct.ac.jp>
2025-07-15 13:17:44 +0900

    n

--- leaning/cfiles/daily_sort/6-13/insertion.c
+++ leaning/cfiles/daily_sort/6-13/insertion.c
@@ -3,9 +3,12 @@
 void insertion_sort(int *a, size_t n){
     if(n < 2) return;
 
+    //頭から末尾まで走査する
     for(size_t i = 0; i < n; i++){
+    //keyを比較して整列ずみの場所の適切な場所に挿入する
         int key = a[i];
         size_t j = i;
+        //jが範囲外アクセスしないかつ，一個前の要素がkeyより大きいならずらす
         for(; j > 0 && a[j - 1] > key; j--) a[j] = a[j - 1];
         a[j] = key;
     }
